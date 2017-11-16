2f4e41d6557858fcf6f5bbfc0026ec520fc5c5a2
Yen Tran <tran.yentran@gmail.com>
2017-11-15 21:43:05 -0800

    Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Strawberry shortcake', 'Make this strawberry shortcake to impress!', 'https://sallysbakingaddiction.com/wp-content/uploads/2017/05/homemade-strawberry-shortcake.jpg')
+    new Recipe('Strawberry shortcake', 'Make this strawberry shortcake to impress!', 'https://sallysbakingaddiction.com/wp-content/uploads/2017/05/homemade-strawberry-shortcake.jpg'),
+    new Recipe('Banana Bread', 'The best banana bread ever!', 'http://www.liveincostarica.com/blog/wp-content/uploads/2013/05/Buttermilk-Banana-Bread.jpg')
   ];
 
   constructor() { }
